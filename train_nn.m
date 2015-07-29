4840ae51903e304e9b5ba89407ccdce5d6da17cf
Katrina Teaño <kteano2006@my.fit.edu>
2015-07-29 14:10:11 -0400

    Creating new training .m file

---
+++ train_nn.m
@@ -0,0 +1,61 @@
+%% Katrina-Mae Teaño
+%% MNIST Digits Kaggle Competition
+%% As adapted from Andrew Ng's Machine Learning assignment
+
+%% Initialization
+clear ; close all; clc
+
+%% Setup parameters 
+input_layer_size  = 28 .^ 2;  % 28x28 Input Images of Digits
+hidden_layer_size = 25;       % 25 hidden units
+num_labels = 10;              % 10 labels, from 1 to 10   
+                              % (note that we have mapped "0" to label 10)
+
+%% =========== Part 1: Loading Data =============
+
+% Load Training Data
+
+load('train.mat');
+m = size(X, 1);
+
+%% ================ Part 2: Loading Pameters ================
+% In this part of the exercise, we load some pre-initialized 
+% neural network parameters.
+
+fprintf('\nLoading Saved Neural Network Parameters ...\n')
+
+% Load the weights into variables Theta1 and Theta2
+load('ex3weights.mat');
+
+%% ================= Part 3: Implement Predict =================
+%  After training the neural network, we would like to use it to predict
+%  the labels. You will now implement the "predict" function to use the
+%  neural network to predict the labels of the training set. This lets
+%  you compute the training set accuracy.
+
+pred = predict(Theta1, Theta2, X);
+
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
+%  To give you an idea of the network's output, you can also run
+%  through the examples one at the a time to see what it is predicting.
+
+%  Randomly permute examples
+rp = randperm(m);
+
+for i = 1:m
+    % Display 
+    fprintf('\nDisplaying Example Image\n');
+    displayData(X(rp(i), :));
+
+    pred = predict(Theta1, Theta2, X(rp(i),:));
+    fprintf('\nNeural Network Prediction: %d (digit %d)\n', pred, mod(pred, 10));
+    
+    % Pause
+    fprintf('Program paused. Press enter to continue.\n');
+    pause;
+end
+
